6e064b190069f11913d3f138bfc50db997fb9d3c
gintaku <trandinhphuoc1198@gmail.com>
2026-08-11 12:10:30 +0900

    fix: correct syntax for kubernetes block in helm provider configuration

--- live/hub/backend.tf
+++ live/hub/backend.tf
@@ -59,7 +59,7 @@ data "aws_eks_cluster_auth" "this" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
